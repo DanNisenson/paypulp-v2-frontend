068273befe3268d096bfe5a44a5d39a96be1699d
DanNisenson <danpablo1@gmail.com>
2023-03-28 16:47:14 +0200

    👷Add paypulp logo to funds card

--- src/Components/Elements/CardImageFunds.jsx
+++ src/Components/Elements/CardImageFunds.jsx
@@ -1,13 +1,14 @@
-import { userContext } from 'Context/UserContext'
-import { useContext } from 'react'
+import PayPulpLogo from 'Components/Elements/Icons/PayPulpLogo'
 import 'Styles/CardImage.css'
 
 const CardImageFunds = ({ funds, position }) => {
   return (
     <div className={`card-funds card-funds__gradient ${position}`}>
       <div className="card-funds__top-info">
-        <div className="card-funds__text">PAYPULP COIN</div>
-        <div className="card-funds__text">Logo</div>
+        <h2 className="card-funds__top-text">PAYPULP COIN</h2>
+        <div className="card-funds__logo">
+          <PayPulpLogo size="36" />
+        </div>
       </div>
       <div className="card-funds__number">Saldo disponible {funds} €</div>
       <div className="card-funds__bottom-info">
