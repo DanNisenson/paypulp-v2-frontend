c1065ba55e8af01d04eb5d72be3c0a0915e4b1b0
DanNisenson <danpablo1@gmail.com>
2023-03-29 14:26:38 +0200

    🗄Add sass to package.json and imports to index and App

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
 import UserContext from './Context/UserContext'
-import './Styles/index.css'
+import './Styles/index.scss'
 
 ReactDOM.render(
   <React.StrictMode>
