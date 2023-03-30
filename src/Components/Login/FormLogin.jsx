54076019d43fdaa6b94cd0e920652a711dbfd22c
DanNisenson <danpablo1@gmail.com>
2023-03-30 19:02:33 +0200

    👷Discard social login

--- src/Components/Login/FormLogin.jsx
+++ src/Components/Login/FormLogin.jsx
@@ -21,24 +21,21 @@ const FormLogin = ({ setBuyerToken }) => {
     <>
       <form className="login__form" onSubmit={handleSubmit(onSubmit)}>
         <h2 className="login__call">Inicia sesión para continuar</h2>
-        <div className="login-form__inputs">
-          <TextInput name="email" label="Email" register={register} />
-          <TextInput type="password" name="password" label="Contraseña" register={register} />
-        </div>
+        {/* <div className="login-form__inputs"> */}
+        <TextInput className="login__email" name="email" label="Email" register={register} />
+        <TextInput type="password" name="password" label="Contraseña" register={register} />
+        {/* </div> */}
         <div className="login__forgot-pass">
           <button className="btn-text-only">Olvide mi contraseña</button>
         </div>
-        <div className="login-form__buttons">
-          <button className="login-form__login-btn btn btn-solid btn-long" onClick={handleSubmit}>
-            Iniciar Sesión
-          </button>
-          <button className="login-form__register-btn btn btn-text-only" onClick={goToRegister}>
-            Registrarse
-          </button>
-        </div>
-        <div className="login__social">
-          <SocialLogos />
-        </div>
+        {/* <div className="login-form__buttons"> */}
+        <button className="login-form__login-btn btn btn-solid btn-long" onClick={handleSubmit}>
+          Iniciar Sesión
+        </button>
+        <button className="login-form__register-btn btn btn-text-only" onClick={goToRegister}>
+          Registrarse
+        </button>
+        {/* </div> */}
       </form>
     </>
   )
